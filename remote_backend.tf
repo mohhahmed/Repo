e61e728d7dba48a14c2c00e7081989ff7865882b
mohhahmed <105142680+mohhahmed@users.noreply.github.com>
2025-04-03 16:56:21 -0500

    Update remote_backend.tf

--- remote_backend.tf
+++ remote_backend.tf
@@ -2,12 +2,12 @@
 # Do not delete this file!
 # It's required to complete the Instruqt labs.
 
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "Sulaimanismail"
-    workspaces {
-      name = "hashicat-aws"
-    }
-  }
-}
+# terraform {
+ # backend "remote" {
+  #  hostname = "app.terraform.io"
+   # organization = "Sulaimanismail"
+    # workspaces {
+     # name = "hashicat-aws"
+    #}
+  #}
+#}
